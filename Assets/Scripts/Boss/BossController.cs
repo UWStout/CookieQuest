cc07c0e6ac4d526af488e788b8ac02b02c7fbef1
Blake <blakejbissell@gmail.com>
2020-02-09 10:28:38 -0600

    Moving Boss

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossController : MonoBehaviour
 {
+    public int health;
+    public int damage;
+
+    public Slider healthBar;
     // Start is called before the first frame update
     void Start()
     {
-     
-
-
+        health = 10;
+        healthBar.maxValue = health;
     }
+
+
+
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthBar.value = health; 
     }
 }
