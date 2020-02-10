b9b94c8c972db0d715fe09598c04d1fc62aa222a
cranea7469 <cranea7469@my.uwstout.edu>
2020-02-10 13:02:33 -0600

    New way to Jump and Move
    I created 2 new scripts to try a different way to move and jump player

---
+++ Assets/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    private Player_Base playerBase;
+    private Rigidbody2D rigidbody2d;
+    
+    private void Awake()
+    {
+        playerBase = gameObject.GetComponent<Player_Base>();
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 10f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+}
