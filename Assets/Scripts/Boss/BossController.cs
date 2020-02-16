4671651c0a59d88964dd18cb2aa1bcb79990fcdd
Blake <blakejbissell@gmail.com>
2020-02-16 13:19:44 -0600

    Revert "Merge branch 'master' of https://github.com/UWStout/CookieQuest"
    This reverts commit 8838c40cb36f4d61ddabb90b0a7dc842da8478ca, reversing
    changes made to b8f88fe77d0a3dd56b9a9ee4a2992aa1f3b711df.

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -5,125 +5,31 @@ using UnityEngine.UI;
 
 public class BossController : MonoBehaviour
 {
-    //internals 
     public int health;
     public int damage;
-    private bool isFirst;
 
-    //UI
     public Slider healthBar;
 
-    //Components of this object
-    public Animator anim;
-
-    //State Machine 
-    private float timer;
-    public float minTime;
-    public float maxTime;
-    public GameObject player;
-    private Transform playerTrans;
-
-
-    //rolling
-    public float rollSpeed;
-   
-    private enum State {
-        Idle,
-        Rolling,
-        Throwing
-    }
-
-    private State bossState;
-
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         health = 10;
-        //healthBar.maxValue = health;
-        //anim = gameObject.GetComponent<Animator>();
-        //player = GameObject.Find("Player");
-        bossState = State.Idle;
-        isFirst = true;
-        
+        healthBar.maxValue = health;
     }
-   
+
+
+
+    
+
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        //Debug.Log(timer);
-        switch(bossState)
+        if(health <= health/2)
         {
-            case State.Idle:
-                if (isFirst == true)
-                {
-                    timer = 3;
-                    isFirst = false;
-                    anim.ResetTrigger("Rolling");
-                    //Debug.Log("Timer Reset");
-                }
-                idleState();
-                timer -= Time.deltaTime;
-                //Debug.Log("IDLE");
-                break;
-            case State.Rolling:
-                if (isFirst == true)
-                {
-                    timer = 3;
-                    isFirst = false;
-                    anim.ResetTrigger("Idle");
-                    //Debug.Log("Timer Reset");
-                }
-                rollingState();
-                timer -= Time.deltaTime;
-                //Debug.Log("ROLLING");
-                break;
-            case State.Throwing:
-                
-                break;
-
+            anim.SetTrigger("Stage2");
         }
-        //healthBar.value = health;
-        
-    }
-
-    private void idleState()
-    {
-        anim.SetTrigger("Idle");
-        if (timer <= 0)
-        {
-            bossState = State.Rolling;
-            isFirst = true;
-        }
-        
-    }
-
-    private void rollingState()
-    {
-        
-        //playerTrans = player.GetComponent<Transform>();
-        anim.SetTrigger("Rolling");
-        Vector2 target = new Vector2(player.transform.position.x, gameObject.transform.position.y);
-        Vector2 playerPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
-        Vector2 rollDir = target - playerPos;
-
-
-        gameObject.transform.localRotation = Quaternion.Euler(gameObject.transform.localRotation.x, gameObject.transform.localRotation.y, gameObject.transform.localRotation.z + 20);
-
-        //gameObject.transform.Translate(rollDir * rollSpeed * Time.deltaTime);
-
-        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, target, rollSpeed * Time.deltaTime);
-
-        if (timer <= 0)
-        {
-            bossState = State.Idle;
-            isFirst = true;
-        }
-
-
-    }
-    private float randomTimer()
-    {
-        float f = Random.Range(minTime, maxTime);
-        return f;
+        healthBar.value = health; 
     }
 }
--- Assets/Scripts/Boss/rollingBehavior.cs
+++ Assets/Scripts/Boss/rollingBehavior.cs
@@ -9,37 +9,46 @@ public class rollingBehavior : StateMachineBehaviour
     public float maxTime;
     public Sprite rollingSprite;
 
-    private GameObject parent;
+    private Transform cookie;
     private Transform trans;
     private Transform playerPos;
     private SpriteRenderer displayedSprite;
     public float speed;
-
+    private Vector2 target;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         timer = Random.Range(minTime, maxTime);
         displayedSprite = GameObject.FindObjectOfType<SpriteRenderer>();
+        target = new Vector2(playerPos.position.x, animator.transform.position.y);
+        cookie = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Transform>();
+       
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        displayedSprite.sprite = rollingSprite;
+        //displayedSprite.sprite = rollingSprite;
         if(timer <= 0)
         {
-            animator.SetTrigger("idle");
+            animator.SetTrigger("Idle");
         } else
         {
             timer -= Time.deltaTime;
         }
 
-        Vector2 target = new Vector2(playerPos.position.x, animator.transform.position.y);
-        Vector2 cookie = new Vector2(animator.transform.position.x, animator.transform.position.y);
-        float direction;
-        direction = Vector2.Dot(cookie, target);
-        animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);   
+        
+        Vector2 cookiePos = new Vector2(animator.transform.position.x, animator.transform.position.y);
+        Vector2 rollDir = target - cookiePos;
+
+
+        cookie.transform.localRotation = Quaternion.Euler(cookie.transform.localRotation.x, cookie.transform.localRotation.y, cookie.transform.localRotation.z + 1);
+
+
+        //animator.transform.Translate(direction * (speed * Time.deltaTime);
+        //animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);
+        animator.transform.Translate(rollDir * speed * Time.deltaTime);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
