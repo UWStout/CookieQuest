549721a616914477a433a8c5fddfd2c52a4898ec
cranea7469 <cranea7469@my.uwstout.edu>
2020-02-17 14:15:05 -0600

    Created flip and because grounding didn't work I changed it to a timer. working on animations

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,14 +9,21 @@ public class player : MonoBehaviour
     private Player_Base playerBase;
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxCollider2d;
-    private Animator anim;
+    private Animator move;
+
+    float jumpCooldown = .45f;
+    float timeSinceAction = 0.0f;
+  
 
     private player p;
     public bool isGrounded;
+    public bool facingRight;
+    float groove = Input.GetAxis("Horizontal;");
+
     
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        move = GetComponent<Animator>();
         playerBase = gameObject.GetComponent<Player_Base>();
         rigidbody2d = transform.GetComponent<Rigidbody2D>();
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
@@ -25,17 +32,27 @@ public class player : MonoBehaviour
     //the jump for the dinosaur if it is grounded and up-arrow pressed
     private void Update()
     {
+
+
         //when collides, it is grounded function
-        isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f), new Vector2(transform.position.x + 0.5f, transform.position.y + 0.5f), platformsLayerMask);
+        // isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f), new Vector2(transform.position.x + 0.5f, transform.position.y + 0.5f), platformsLayerMask);
+
+        timeSinceAction += Time.deltaTime;
 
         //if grounded == true and up arrow pressed then he will jump
-        if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
+        if (timeSinceAction > jumpCooldown && Input.GetKeyDown(KeyCode.UpArrow))
         {
+            timeSinceAction = 0;
             float jumpVelocity = 10f;
             rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            move.SetBool("Moving", true);
         }
 
         LeftRightMovement();
+
+
+        
+
 
     }
 
@@ -43,38 +60,56 @@ public class player : MonoBehaviour
     //movement plus animation plus flip
     private void LeftRightMovement()
     {
+
+        Vector2 characterscale = transform.localScale;
+        
+
         float movespeed = 5f;
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-//<<<<<<< HEAD
            // anim.SetTrigger("walking");
-//=======
-            //anim.SetTrigger("walking");
-//>>>>>>> 4461456583b635750f822bfde2b5839afa2853ff
             rigidbody2d.velocity = new Vector2(-movespeed, rigidbody2d.velocity.y);
+            // move.SetBool("Moving", true);
+            //flips left if moving left
+            if (groove > 0)
+            {
+                Flip();
+            }
+            
+            characterscale.x = -1;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-//<<<<<<< HEAD
            // anim.SetTrigger("Walking");
-//=======
-           //anim.SetTrigger("Walking");
-//>>>>>>> 4461456583b635750f822bfde2b5839afa2853ff
             rigidbody2d.velocity = new Vector2(+movespeed, rigidbody2d.velocity.y);
+            //move.SetBool("Moving", true);
+            //Flips right if moving right;
+            if (groove < 0)
+            {
+                Flip();
+            }
+            characterscale.x = 1;
+
         }
         else
         {
-//<<<<<<< HEAD
            // anim.SetTrigger("idle");
-//=======
-            //anim.SetTrigger("idle");
-//>>>>>>> 4461456583b635750f822bfde2b5839afa2853ff
             rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
         }
+
+        transform.localScale = characterscale;
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
     }
 
 
+  
     
 
 }
