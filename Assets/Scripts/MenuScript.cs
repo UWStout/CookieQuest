e43744259014fab479d832509c47d515f51c0283
cranea7469 <cranea7469@my.uwstout.edu>
2020-02-21 13:50:03 -0600

    idk

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Quit()
+    {
+        //For use in editor.
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //Uncomment if building for final build
+        //Application.Quit();
+
+    }
+
+}
