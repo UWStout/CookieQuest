d99006483113a8b63fa6a5a088e0cf4d975e5620
Blake <blakejbissell@gmail.com>
2020-02-23 16:18:26 -0600

    Final Working Commit

---
+++ Assets/Scripts/WonScreenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WonScreenScript : MonoBehaviour
+{
+    public void Retry(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        //For use in editor.
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //Uncomment if building for final build
+        //Application.Quit();
+
+    }
+}
--- Assets/Scripts/playerHealth.cs
+++ Assets/Scripts/playerHealth.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class playerHealth : MonoBehaviour
 {
+    public Slider healthBar;
     public int maxHealth = 10;
     public int currentHealth;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+    }
+
+    public void Update()
+    {
+        healthBar.value = currentHealth;
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene(3);
+        }
     }
 
     public void TakeDamage(int damage)
